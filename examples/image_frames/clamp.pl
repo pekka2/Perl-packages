4f11d7c556d40d086a421396fd8ae79f250a0f12
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-03-06 13:38:15 +0200

    Add files via upload

--- examples/image_frames/clamp.pl
+++ examples/image_frames/clamp.pl
@@ -1,1 +1,17 @@
+#!C:\Perl64\bin\perl.exe
+use GD;
+use GD::Frame;
+use CGI::Carp qw(fatalsToBrowser);
 
+print "Content-type:image/png\n\n";
+
+my $frame = newFromPng GD::Image("images/frame.png");
+my ($width2,$height2) = $frame->getBounds();
+my $src = newFromPng GD::Image("images/photo.png");
+my ($width,$height) = $src->getBounds();
+my $frame_width = 20;
+my $image = GD::Frame->clamp($frame,$src,$frame_width,$width,$height,$width2,$height2);
+
+binmode STDOUT;
+
+print $image->png;
