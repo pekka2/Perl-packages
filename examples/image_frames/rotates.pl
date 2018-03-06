93155de1a585413892708d57919987830f600a80
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-03-06 13:26:48 +0200

    Add files via upload

---
+++ examples/image_frames/rotates.pl
@@ -0,0 +1,20 @@
+#!C:\Perl64\bin\perl.exe
+use GD;
+use CGI::Carp qw(fatalsToBrowser);
+use GD::FullRotate;
+print "Content-type:text/html\n\n";
+
+my $src =  newFromPng GD::Image("niitty.png");
+my ($width,$height) = $src->getBounds();
+
+my $angle = 132;
+my @color = (255,255,255);
+my $transparent = 1;
+my $image = new GD::FullRotate($src,$width,$height,$angle,$transparent,@color);
+
+my $file = "testif.png";
+open OUT, '>:raw', $file or die $!;
+print OUT $image->png;
+close OUT;
+
+system $file;
