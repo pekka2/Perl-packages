b66c32395d90b9f3d89c2eebe4976803d51dfa66
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-03-06 13:38:58 +0200

    Update clamp.pl

--- examples/image_frames/clamp.pl
+++ examples/image_frames/clamp.pl
@@ -9,7 +9,7 @@ my $frame = newFromPng GD::Image("images/frame.png");
 my ($width2,$height2) = $frame->getBounds();
 my $src = newFromPng GD::Image("images/photo.png");
 my ($width,$height) = $src->getBounds();
-my $frame_width = 20;
+my $frame_width = 100;
 my $image = GD::Frame->clamp($frame,$src,$frame_width,$width,$height,$width2,$height2);
 
 binmode STDOUT;
