d871d2fdc5a778d0984725cbdfad0beb26ec62e2
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-03-06 13:45:34 +0200

    Add files via upload

--- examples/billing_sheet/bill.pl
+++ examples/billing_sheet/bill.pl
@@ -9,16 +9,15 @@ use strict;
 use warnings;
 use Excel::Writer::XLSX::Bill;
 
-$file = 'invoice.xlsx'; # create to this file
-# language options are english, 'en' and finnish, 'fi'
-$language = 'en';
-$font = 'Arial Bold';
-$heading_font_size = 18;
-$content_font_size = 11;
-$small_font_size = 8;
-$totals_font_size = 13;
-# images size can be maxim about 175x35 pixels
-$image = '';
+# Create a new workbook and add a worksheet
+my $file = 'invoice.xlsx';
+my $language = 'en';
+my $font = 'Arial Bold';
+my $heading_font_size = 18;
+my $content_font_size = 11;
+my $small_font_size = 8;
+my $totals_font_size = 13;
+my $image = '';
 my $workbook  = new Excel::Writer::XLSX::Bill($file,
 	                                          $language,
 	                                          $font,
