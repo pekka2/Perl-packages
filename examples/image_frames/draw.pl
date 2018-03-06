3de68db7786e33ad4fb8cb084a2c01c4222ad4ac
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-03-06 13:34:37 +0200

    Add files via upload

--- examples/image_frames/draw.pl
+++ examples/image_frames/draw.pl
@@ -1,1 +1,17 @@
+#!C:\Perl64\bin\perl.exe
+use GD;
+use GD::Frame;
+use CGI::Carp qw(fatalsToBrowser);
 
+print "Content-type:image/jpeg\n\n";
+
+my $frame = newFromJpeg GD::Image("images/100.jpg");
+my ($width2,$height2) = $src->getBounds();
+my $src = newFromJpeg GD::Image("images/photo.jpg");
+my ($width,$height) = $src->getBounds();
+my $frame_width = 100;
+my $image = GD::Frame->draw($frame,$src,$frame_width,$width,$height);
+
+binmode STDOUT;
+
+print $image->jpeg;
