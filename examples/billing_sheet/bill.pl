bc97996052e8ef20c50075aceabec5c5977fbf59
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-03-06 12:53:14 +0200

    Add files via upload

--- examples/billing_sheet/bill.pl
+++ examples/billing_sheet/bill.pl
@@ -1,1 +1,29 @@
+#!C:\Perl64\bin\perl.exe
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
+use Data::Dumper;
+my $query = new CGI;
 
+print "Content-type:text/html\n\n";
+use strict;
+use warnings;
+use Excel::Writer::XLSX::Bill;
+
+# Create a new workbook and add a worksheet
+$file = 'invoice.xlsx';
+$language = 'en';
+$font = 'Arial Bold';
+$heading_font_size = 18;
+$content_font_size = 11;
+$small_font_size = 8;
+$totals_font_size = 13;
+$image = '';
+my $workbook  = new Excel::Writer::XLSX::Bill($file,
+	                                          $language,
+	                                          $font,
+	                                          $heading_font_size,
+	                                          $content_font_size,
+	                                          $small_font_size,
+	                                          $totals_font_size,
+	                                          $image
+	                                          );
