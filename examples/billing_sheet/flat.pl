34c3935234e06d2e5c1fa6b1bff70f6158d0c12c
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-03-09 12:45:41 +0200

    Update flat.pl

--- examples/billing_sheet/flat.pl
+++ examples/billing_sheet/flat.pl
@@ -9,15 +9,10 @@ use strict;
 use warnings;
 use Excel::Writer::XLSX::Bill;
 
-# Create a new workbook and add a worksheet
 my $file = 'bill-22.xlsx';
 my $language = 'fi';
-my $font = 'Arial Bold';
-my $heading_font_size = 18;
-my $content_font_size = 11;
-my $small_font_size = 8;
-my $totals_font_size = 13;
-my $image = 'logo2.jpg';
+# maxim image size about 235x70 pixels
+my $image = 'invoice-logo.jpg';
 my $workbook  = Excel::Writer::XLSX::Bill->flat($file,
 	                                          $language,
 	                                          $image
